1d0d73f73198291dbe0869633380bb64e148e985
vbloke <vbloke@gmail.com>
2017-08-15 10:37:45 +0100

    logo transition

--- header.php
+++ header.php
@@ -3,8 +3,6 @@
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-<meta name="apple-mobile-web-app-capable" content="yes">
-<meta name="mobile-web-app-capable" content="yes">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <?php wp_head(); ?>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" />
