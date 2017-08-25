d0ca8f2bd8680638893fe7b06b65e5816dcb9b69
vbloke <vbloke@gmail.com>
2017-08-25 08:08:26 +0100

    removed people from search

--- search.php
+++ search.php
@@ -3,7 +3,7 @@
 <div class="row expanded" id="wwwwPage">
 
 	<div class="small-12 columns">
-		<h2>Search for "<?php the_search_query(); ?>"</h2>
+		<h2>You searched for "<?php the_search_query(); ?>"</h2>
 	</div>
 
 </div>
@@ -12,7 +12,7 @@
 
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-		<div class="small-12 medium-6 large-3 columns animated fadeInUp">
+		<div class="columns animated fadeInUp">
 			<div class="card">
 				<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('card'); ?></a>
 				<div class="card-divider">
